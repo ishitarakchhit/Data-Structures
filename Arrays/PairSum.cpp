c377e6459475c0287ebf5cdd80aa3964f1967551
ishitarakchhit <104856237+ishitarakchhit@users.noreply.github.com>
2022-11-06 20:29:43 +0530

    Create PairSum.cpp
    https://www.codingninjas.com/codestudio/problems/pair-sum_697295?source=youtube&campaign=love_babbar_codestudio1&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio1

---
+++ Arrays/PairSum.cpp
@@ -0,0 +1,25 @@
+/*
+Each pair should be sorted in increasing order i.e. the pairs are sorted according to the first element of each pair and if they are same then according to the 2nd element
+The elements inside each pair should be in increasing order.
+*/
+
+#include <bits/stdc++.h> 
+#include <bits/stdc++.h> 
+vector<vector<int>> pairSum(vector<int> &arr, int s){
+   // Write your code here.
+    vector <vector <int>> ans;
+    
+    for(int i=0; i<arr.size(); i++){
+        for(int j=i+1; j<arr.size(); j++){
+            
+            if(arr[i]+arr[j]==s){
+                vector <int> temp;
+               temp.push_back(min(arr[i],arr[j]));
+                temp.push_back(max(arr[i],arr[j]));
+                ans.push_back(temp);
+            }
+        }
+    }
+    sort(ans.begin(), ans.end());
+    return ans;
+}
