9aa5838700fa80bfdf432aa95b8e029a29617eee
ishitarakchhit <104856237+ishitarakchhit@users.noreply.github.com>
2022-11-27 17:41:17 +0530

    Create Light Up the Street!.cpp
    https://www.codingninjas.com/codestudio/contests/love-babbar-contest-ii/3583551/problems/14751

---
+++ Practice/Arrays/Light Up the Street!.cpp
@@ -0,0 +1,6 @@
+/*
+You are walking on a street of length 'N'. You want to light up the street by installing street lights.
+There are 'M' streetlights and the 'ith' streetlight can be installed at position 'Ci'.
+You are also given the range of the streetlights 'R' which means that the 'ith' streetlight installed at position 'Ci' will enlighten street from 'Ci-R' to 'Ci+R'.
+Output the minimum number of street lights required to light up the whole street or output -1 of it is impossible to light the whole street inder the given consitions. 
+*/
