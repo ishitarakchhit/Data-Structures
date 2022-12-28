05ab0909227566c5a4c97d8a9419f1e5bff4bfdd
ishitarakchhit <104856237+ishitarakchhit@users.noreply.github.com>
2022-12-29 00:38:21 +0530

    Update Find All Duplicates in an Array.cpp

--- DSA/Lecture 010: Solving LeetCode | CodeStudio Questions [Arrays]/Find All Duplicates in an Array.cpp
+++ DSA/Lecture 010: Solving LeetCode | CodeStudio Questions [Arrays]/Find All Duplicates in an Array.cpp
@@ -1,3 +1,4 @@
+(https://leetcode.com/problems/find-all-duplicates-in-an-array/description/)
 /*
 Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
 
@@ -11,3 +12,21 @@ Input: nums = [4,3,2,7,8,2,3,1]
 Output: [2,3]
 */
 
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector <int> ans;
+        
+        int a[100000]={0};
+
+        for(int i=0; i<nums.size(); i++){
+            if(a[nums[i]-1]){
+                ans.push_back(nums[i]);
+                continue;
+            }
+            a[nums[i]-1]=1;
+        }
+
+        return ans;
+    }
+};
