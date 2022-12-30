c37bb6b6693e096b0141a9792f608c85463e490a
ishitarakchhit <104856237+ishitarakchhit@users.noreply.github.com>
2022-12-30 17:59:53 +0530

    Update Sort 012.cpp

--- DSA/Lecture 010: Solving LeetCode | CodeStudio Questions [Arrays]/Sort 012.cpp
+++ DSA/Lecture 010: Solving LeetCode | CodeStudio Questions [Arrays]/Sort 012.cpp
@@ -1,7 +1,9 @@
+[https://www.codingninjas.com/codestudio/problems/sort-0-1-2_631055?leftPanelTab=0]
 /*
 Sort an array which has elements 0, 1 and 2 while traversing the array only once
 */
-
+//Solution
+//always keeping the left pointer at 0 or just one place after 0 and always keeping the right pointer at 2 or just one place left to 2 so that whenever mid pointer encounters a 0 it can swap the value with left pointer and whenever it encounters a 2 it can swap the value with the right pointer.
 #include <bits/stdc++.h> 
 void sort012(int *arr, int n)
 {
@@ -24,3 +26,36 @@ void sort012(int *arr, int n)
     
    
 }
+ 
+ //alternate solution
+ //count all the 0s, 1s and 2s and store all of them sequencially in the array.
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   //   Write your code here
+    int count0=0; 
+    int count1=0;
+    int count2=0;
+ 
+    for(int i=0; i<n; i++){
+        if(arr[i]==0)
+            count0++;
+        else if (arr[i]==1)
+            count1++;
+        else 
+            count2++;        
+    }
+    
+    int i=0;
+    int num=0;
+    
+    while(i<n){
+        if(i==count0)
+            num=1;
+        if(i==count0+count1)
+            num=2;
+        arr[i]=num;
+        i++;
+    }
+}
+ 
